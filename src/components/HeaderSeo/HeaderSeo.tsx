04b7b93841141be59174f2b6d2b0eda25556261d
Rafa Tárrega <rafatarre@gmail.com>
2023-05-07 16:33:44 +0200

    feat: change seo title and description

--- src/components/HeaderSeo/HeaderSeo.tsx
+++ src/components/HeaderSeo/HeaderSeo.tsx
@@ -6,10 +6,10 @@ export const HeaderSeo = (props: HeaderSeoProps) => {
 
   return (
     <Head>
-      <title>{title ? title : "Rafa Tárrega • TarreDev"}</title>
+      <title>{title ? title : "Rafa Tárrega • TarreDev | Frontend, Javascript, React y Tips sobre desarrollo"}</title>
       <meta
         name="description"
-        content={description ? description : "Desarrollador Frontend"}
+        content={description ? description : "Artículos sobre programación, tutoriales y recursos sobre el desarrollo web, javascript, React y tips sobre programación"}
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
