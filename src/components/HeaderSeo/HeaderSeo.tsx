19ce9eba9779aa312304afbe1876849986495612
Rafa Tárrega <rafatarre@gmail.com>
2023-05-07 16:37:10 +0200

    feat: search console

--- src/components/HeaderSeo/HeaderSeo.tsx
+++ src/components/HeaderSeo/HeaderSeo.tsx
@@ -11,6 +11,7 @@ export const HeaderSeo = (props: HeaderSeoProps) => {
         name="description"
         content={description ? description : "Desarrollador Frontend"}
       />
+      <meta name="google-site-verification" content="UsdOvrJeUrELBDna7yT1tQPb222s4y96lfdH_nBswRU" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
       <meta property="og:locale" content="es_ES" />
