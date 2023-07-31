93602842e3a713e94908b5021acc1734f53781a9
Rafa Tárrega <rafatarre@gmail.com>
2023-07-31 12:06:33 +0200

    fix: name typescript uppercase

--- src/components/Pages/Recomendaciones/Recomendations.tsx
+++ src/components/Pages/Recomendaciones/Recomendations.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
 import { dataRecomendations } from "./Recomendations.data";
+import Atropos from 'atropos/react';
 
 export function RecomendationsPage() {
   return (
-    <main className="mt-5 px-5 lg:mt-20 lg:pr-10">
-      <h1 className="max-w-4xl text-4xl font-bold  text-textLight">
+    <main className="px-5 mt-5 lg:mt-20 lg:pr-10">
+      <h1 className="max-w-4xl text-4xl font-bold text-textLight">
         El software que uso, los dispositivos me encantan, podcast que escucho y
         otras cosas que recomiendo
       </h1>
-      <h3 className="mt-4 max-w-4xl  text-zinc-400">
+      <h3 className="max-w-4xl mt-4 text-zinc-400">
         Las cosas que uso para crear webs, mantenerme productivo o comprar para
         engañarme y pensar que estoy siendo productivo cuando en realidad solo
         estoy procrastinando. Aquí hay una gran lista de todas mis cosas
@@ -25,9 +26,12 @@ export function RecomendationsPage() {
             <h3 className="my-6 text-textLight">{data.name}</h3>
             <div className="grid grid-cols-2 gap-x-5 md:grid-cols-5">
               {data.values.map((value) => (
-                <div
+                <Atropos
                   key={value.name}
-                  className="relative mb-10 w-full  rounded-lg border border-white/10	bg-zinc-900/20 p-4 text-textLight transition-transform duration-500 hover:scale-105"
+                  highlight={false}
+                  activeOffset={20}
+                  shadowScale={0.5}
+                  className="relative w-full p-4 mb-10 transition-transform duration-500 border rounded-lg border-white/10 bg-zinc-900/20 text-textLight "
                 >
                   <Image
                     src={value.image}
@@ -36,16 +40,16 @@ export function RecomendationsPage() {
                     height="90"
                     className="m-auto rounded-lg"
                   />
-                  <p className="mt-3 text-center text-sm text-textLight line-clamp-2">
+                  <p className="mt-3 text-sm text-center text-textLight line-clamp-2">
                     {value.name}
                   </p>
                   <Link
                     href={value.url}
-                    className="absolute right-0 bottom-0 h-full w-full"
+                    className="absolute bottom-0 right-0 w-full h-full"
                     target="_blank"
                     rel="noreferrer"
                   />
-                </div>
+                </Atropos>
               ))}
             </div>
           </section>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/post.css";
 import NextNProgress from 'nextjs-progressbar';
+import 'atropos/css'
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
