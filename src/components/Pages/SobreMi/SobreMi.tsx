990f23105181394f8922331424c4a85a6272007c
Rafa Tárrega <rafatarre@gmail.com>
2024-02-25 09:39:01 +0100

    fix: link about me

--- src/components/Pages/SobreMi/SobreMi.tsx
+++ src/components/Pages/SobreMi/SobreMi.tsx
@@ -6,14 +6,14 @@ export function SobreMiPage() {
   return (
     <main className="mx-auto min-h-[75vh] max-w-6xl">
       <div className="mt-5 lg:mt-20">
-        <div className="justify-around 	md:flex lg:py-5">
-          <div className=" flex-1 px-5">
-            <h1 className="bold max-w-sm bg-gradient-to-r from-indigo-500 to-orange-500 bg-clip-text text-5xl font-bold text-textLight text-transparent">
+        <div className="justify-around md:flex lg:py-5">
+          <div className="flex-1 px-5 ">
+            <h1 className="max-w-sm text-5xl font-bold text-transparent bold bg-gradient-to-r from-indigo-500 to-orange-500 bg-clip-text text-textLight">
               <i>
                 Con código todo <span className="text-[5rem]">es posible</span>
               </i>
             </h1>
-            <h2 className="max-w-sm pr-4 text-right text-2xl text-textLight">
+            <h2 className="max-w-sm pr-4 text-2xl text-right text-textLight">
               -TarreDev-
             </h2>
             <div className="mt-4 text-lg text-zinc-400">
@@ -34,8 +34,8 @@ export function SobreMiPage() {
                 probadas o aprender con cursos online?
               </p>
               <p className="my-6">Encuentra todos mis cursos disponibles</p>
-              <Link href="/porfolio">
-                <div className="shadowButtonDark mx-auto flex w-fit items-center rounded-md bg-zinc-800 px-5 py-2 transition-transform duration-500 hover:scale-105">
+              <Link href="/cursos">
+                <div className="flex items-center px-5 py-2 mx-auto transition-transform duration-500 rounded-md shadowButtonDark w-fit bg-zinc-800 hover:scale-105">
                   <Image
                     src={`/assets/img/svg/link.svg`}
                     alt="Link"
@@ -58,12 +58,12 @@ export function SobreMiPage() {
               className="mx-auto rounded-xl"
               priority
             />
-            <div className="mt-10 grid grid-cols-2 gap-y-4 ">
+            <div className="grid grid-cols-2 mt-10 gap-y-4 ">
               {dataSocialNetworks.map(({ link, icon, name }) => (
                 <Link
                   key={link}
                   href={link}
-                  className="mx-auto flex items-center text-zinc-400 hover:text-white"
+                  className="flex items-center mx-auto text-zinc-400 hover:text-white"
                   target="_blank"
                 >
                   <Image
